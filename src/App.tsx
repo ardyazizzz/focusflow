2fc38ea9c6a4275ea0d93318820d69f5d56aec8c
ardyazizzz <ardyazizrw@gmail.com>
2026-07-26 15:50:00 +0700

    Revert to min-h-screen, remove overflow-hidden - restores original page scrollbar behavior

--- src/App.tsx
+++ src/App.tsx
@@ -43,7 +43,7 @@ export default function App() {
 
   return (
     <QueryClientProvider client={queryClient}>
-    <div className="h-screen flex flex-col bg-white font-sans antialiased overflow-hidden">
+    <div className="min-h-screen flex flex-col bg-white font-sans antialiased">
       <Toaster position="top-center" richColors />
 
       {/* Header */}
